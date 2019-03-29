10dd319f46bee5a0588ecae5e968dcb805be7c0a
h12695 <t.hnh@tuta.io>
2019-03-30 00:32:21 +1000

    added user output determination & password requirement

--- banking-app.rb
+++ banking-app.rb
@@ -1,4 +1,17 @@
 puts "Welcome to the banking app. Please enter your name."
 name = gets.chomp
 
-puts "Welcome #{name}, what would you like do do? (options: balance)"
+puts "Hi #{name}! Please enter your password below:"
+password = gets.chomp
+if password == "0000"
+    puts "What would you like do do? (options: balance)"
+        optionChose = gets.chomp
+        if optionChose == "balance"
+            puts "Your balance is $0"
+        else
+            puts "Invalid selection!"
+        end
+else
+    puts "Sorry, the password you have entered is incorrect. You have now been locked out."
+    abort
+end
