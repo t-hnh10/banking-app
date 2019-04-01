d45f2e713da855f8b991d3d6bbb6b6fe2f20a3e6
h12695 <t.hnh@tuta.io>
2019-04-01 10:13:03 +1000

    added loop, withdrawal option & balance can be updated

--- banking-app.rb
+++ banking-app.rb
@@ -1,19 +1,33 @@
-puts "Welcome to the banking app. Please enter your name."
+balance = 0.00
+
+puts "Welcome to the online banking service!. Please enter your name."
 name = gets.chomp
 
 puts "Hi #{name}! Please enter your password below:"
 password = gets.chomp
 if password == "0000"
-    puts "What would you like do do? (options: balance, deposit)"
+    loop do
+    puts "What would you like do do? (options: balance, withdraw, deposit, exit)"
         optionChose = gets.chomp
         if optionChose == "balance"
-            puts "Your balance is $0"
+            puts "Your balance is $#{balance}"
         elsif optionChose == "deposit"
             puts "How much would you like to deposit?"
-            depositAmount = gets.chomp
+            depositAmount = gets.chomp.to_f
+            balance = balance+depositAmount
+            puts "Your current balance is $#{balance}."
+        elsif optionChose == "withdraw"
+            puts "How much would you like to withdraw?"
+            withdrawalAmount = gets.chomp.to_f
+            balance = balance-withdrawalAmount
+            puts "Your current balance is $#{balance}."
+        elsif optionChose == "exit"
+            puts "Thanks for choosing our bank. We put customers first!"
+            abort
         else
             puts "Invalid selection!"
         end
+    end
 else
     puts "Sorry, the password you have entered is incorrect. You have now been locked out."
     abort
