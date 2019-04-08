e9302001f207b0447e7e3fb547c2b1edb8b3d180
t-hnh10 <t.hnh@tuta.io>
2019-04-08 15:37:25 +1000

    limited withdraw option, stored transactions in arrays & implemented history option

--- banking-app.rb
+++ banking-app.rb
@@ -1,5 +1,6 @@
 # Variables
 balance = 0.00
+array = []
 
 puts "Welcome to the online banking service!. Please enter your name."
 name = gets.chomp
@@ -8,20 +9,29 @@ puts "Hi #{name}! Please enter your password below:"
 password = gets.chomp
 if password == "0000"
     loop do
-    puts "What would you like to do? (options: balance, withdraw, deposit, exit)"
-        optionChose = gets.chomp
+    puts "What would you like to do? (options: balance, withdraw, deposit, history, exit)"
+        optionChose = gets.chomp.downcase
         if optionChose == "balance"
             puts "Your balance is $#{balance}"
+            array << "Checked balance"
         elsif optionChose == "deposit"
             puts "How much would you like to deposit?"
             depositAmount = gets.chomp.to_f
             balance = balance+depositAmount
             puts "Your current balance is $#{balance}."
+            array << "Deposit: $#{depositAmount}"
         elsif optionChose == "withdraw"
             puts "How much would you like to withdraw?"
-            withdrawalAmount = gets.chomp.to_f
-            balance = balance-withdrawalAmount
-            puts "Your current balance is $#{balance}."
+            withdrawAmount = gets.chomp.to_f
+            if withdrawAmount > balance
+                puts "Error! You are attempting to withdraw an invalid amount."
+            else withdrawAmount <= balance
+                balance = balance-withdrawAmount
+                puts "Your current balance is $#{balance}."
+                array << "Withdraw: $#{withdrawAmount}"
+            end
+        elsif optionChose == "history"
+            puts array
         elsif optionChose == "exit"
             puts "Thanks for choosing our bank. We put customers first!"
             abort
